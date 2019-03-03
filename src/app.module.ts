3b6e3ab044e5f45ecfb7ac5d6a14b958d2a47ff6
Eugene Krakos <krakosevich93@gmail.com>
2019-03-04 01:57:29 +0300

    configuration service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from './core/core.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [CoreModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })

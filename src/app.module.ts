1e5785675a29ca8bbcf388885536d9d97753e160
Eugene Krakos <krakosevich93@gmail.com>
2019-03-04 02:14:43 +0300

    Configuration (#1)
    * configuration service
    
    * mongo db added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from './core/core.module';
+import { ConfigModule } from './config/config.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './config/config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    CoreModule,
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.mongodbUri,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

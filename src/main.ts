7e586617c1eb62a26f7c07c3699621706b789fc0
Eugene Krakos <krakosevich93@gmail.com>
2019-03-10 03:06:53 +0300

    security + optimization

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config/config.service';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, validationError: { target: false, value: false}}));
   const config = app.get('ConfigService') as ConfigService;
+
+  // security
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(csurf({ cookie: true }));
+
+  // optimization
+  app.use(compression());
+
   await app.listen(config.port);
 }
 bootstrap();
